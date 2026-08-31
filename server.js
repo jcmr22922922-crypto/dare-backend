f78d96129005ceab02c6ea1c79105050a05fe6e3
jcmr22922922-crypto <jcmr22922922@gmail.com>
2026-08-31 13:20:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const app = express();
 
